3bfaf44cb8a15b1251b4cf5f15e1b7ebc047e77e
NickSlm <solomonovnick@gmail.com>
2022-01-23 14:32:02 +0200

    Script creates Initial ELK stack configuration files for give hosts

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from copy import copy
 from msilib import sequence
+from operator import le
 import sys
 import json
 import csv
@@ -14,7 +15,7 @@ class Configuration():
     def __init__(self):
         self.hosts = {"kibana":[],"elasticsearch":[]}
         self.hosts_data = {"instances": []}
-        self.docker_compose = {"Version":"2.2","services":{}}
+        self.docker_compose = {"version":"2.2","services":{},"volumes":{"certs":{"driver":"local"}},"networks":{"elastic":{"driver":"bridge"}}}
 
     def add_host(self,host):
         self.hosts_data['instances'].append(host)
@@ -36,26 +37,110 @@ class Configuration():
         with open(rf'{directory_path}\instances.yml', 'w') as file:
             yaml.dump(self.hosts_data, file)
 
-    def create_conf_files(self,directory_path):
+    def create_evn_file(self,directory_path):
         dotenv.set_key(f'{directory_path}\.env',"VERSION","7.16.2")
         dotenv.set_key(f'{directory_path}\.env',"COMPOSE_PROJECT_NAME","es")
         dotenv.set_key(f'{directory_path}\.env',"CERTS_DIR","/usr/share/elasticsearch/config/certificates" )
 
     def create_certs(self,directory_path):
-        copyfile(r'D:\ELK docker-compose script\create_certs.yml',directory_path)
+        copyfile(r"D:\ELK docker-compose script\create-certs.yml",f"{directory_path}\create-certs.yml")
 
-    def create_docker_compose(self):
-        for host in self.hosts_data['instances']:
-            # elasticsearch
-            self.docker_compose['services'].update({host['name']:{"container_name":host['name'],"image": "docker.elastic.co/elasticsearch/elasticsearch:$VERSION",
-            "environment":[f"node.nodename={host['name']}"]}})
+    def create_docker_compose(self,directory_path):
+        if self.hosts_data['instances'][0]['name'] in self.hosts['elasticsearch']:
+                self.docker_compose['volumes'].update({f"{self.hosts_data['instances'][0]['name']}_data":{'driver':'local'}})
+                self.docker_compose['services'].update(
+                    {self.hosts_data['instances'][0]['name']:
+                    {
+                    "image": "docker.elastic.co/elasticsearch/elasticsearch:${VERSION}",
+                    "container_name":self.hosts_data['instances'][0]['name'],
+                    "environment":{
+                        "node.name":f"{self.hosts_data['instances'][0]['name']}",
+                        "cluster.name": "es-docker-cluster",
+                        "discovery.seed_hosts": f"{','.join(self.hosts['elasticsearch'])}",
+                        "cluster.initial_master_nodes": f"{','.join(self.hosts['elasticsearch'])}",
+                        "bootstrap.memory_lock":"true",
+                        "ES_JAVA_OPTS":"-Xms512m -Xmx512m",
+                        "xpack.license.self_generated.type":"trial",
+                        "xpack.security.enabled":"true",
+                        "xpack.security.http.ssl.enabled":"true",
+                        "xpack.security.http.ssl.key":f"$CERTS_DIR/{self.hosts_data['instances'][0]['name']}/{self.hosts_data['instances'][0]['name']}.key",
+                        "xpack.security.http.ssl.certificate_authorities":"$CERTS_DIR/ca/ca.crt",
+                        "xpack.security.http.ssl.certificate":f"$CERTS_DIR/{self.hosts_data['instances'][0]['name']}/{self.hosts_data['instances'][0]['name']}.crt",
+                        "xpack.security.transport.ssl.enabled":"true",
+                        "xpack.security.transport.ssl.verification_mode":"certificate",
+                        "xpack.security.transport.ssl.certificate_authorities":"$CERTS_DIR/ca/ca.crt",
+                        "xpack.security.transport.ssl.certificate":f"$CERTS_DIR/{self.hosts_data['instances'][0]['name']}/{self.hosts_data['instances'][0]['name']}.crt",
+                        "xpack.security.transport.ssl.key":f"$CERTS_DIR/{self.hosts_data['instances'][0]['name']}/{self.hosts_data['instances'][0]['name']}.key",
+                    },
+                    "ulimits":{"memlock":{"soft": -1,"hard": -1}},
+                    "volumes": [f"{self.hosts_data['instances'][0]['name']}_data:/usr/share/elasticsearch/data", "certs:$CERTS_DIR"],
+                    "ports": ['9200:9200'],
+                    "networks":["elastic"],
+                    "healthcheck": {"test": "curl --cacert $CERTS_DIR/ca/ca.crt -s https://localhost:9200 >/dev/null; if [[ $$? == 52 ]]; then echo 0; else echo 1; fi",
+                        "interval": "30s",
+                        "timeout": "10s",
+                        "retries": 5}}})
 
-            # kibana
-        
-        # yaml = YAML()
-        # yaml.explicit_start = True
-        # yaml.indent(sequence=4, offset=2)
-        # with open(r'D:\ELK docker-compose script\docker-compose.yml', 'w') as file:
-        #     yaml.dump(self.docker_compose, file)
+        for host in range(1,len(self.hosts_data['instances'])):
+            # Elasticsearch nodes
+            if self.hosts_data['instances'][host]['name'] in self.hosts['elasticsearch']:
+                self.docker_compose['volumes'].update({f"{self.hosts_data['instances'][host]['name']}_data":{'driver':'local'}})
+                self.docker_compose['services'].update(
+                    {self.hosts_data['instances'][host]['name']:
+                    {
+                    "image": "docker.elastic.co/elasticsearch/elasticsearch:${VERSION}",
+                    "container_name":self.hosts_data['instances'][host]['name'],
+                    "environment":{
+                        "node.name":f"{self.hosts_data['instances'][host]['name']}",
+                        "cluster.name": "es-docker-cluster",
+                        "discovery.seed_hosts": f"{','.join(self.hosts['elasticsearch'])}",
+                        "cluster.initial_master_nodes": f"{','.join(self.hosts['elasticsearch'])}",
+                        "bootstrap.memory_lock":"true",
+                        "ES_JAVA_OPTS":"-Xms512m -Xmx512m",
+                        "xpack.license.self_generated.type":"trial",
+                        "xpack.security.enabled":"true",
+                        "xpack.security.http.ssl.enabled":"true",
+                        "xpack.security.http.ssl.key":f"$CERTS_DIR/{self.hosts_data['instances'][host]['name']}/{self.hosts_data['instances'][host]['name']}.key",
+                        "xpack.security.http.ssl.certificate_authorities":"$CERTS_DIR/ca/ca.crt",
+                        "xpack.security.http.ssl.certificate":f"$CERTS_DIR/{self.hosts_data['instances'][host]['name']}/{self.hosts_data['instances'][host]['name']}.crt",
+                        "xpack.security.transport.ssl.enabled":"true",
+                        "xpack.security.transport.ssl.verification_mode":"certificate",
+                        "xpack.security.transport.ssl.certificate_authorities":"$CERTS_DIR/ca/ca.crt",
+                        "xpack.security.transport.ssl.certificate":f"$CERTS_DIR/{self.hosts_data['instances'][host]['name']}/{self.hosts_data['instances'][host]['name']}.crt",
+                        "xpack.security.transport.ssl.key":f"$CERTS_DIR/{self.hosts_data['instances'][host]['name']}/{self.hosts_data['instances'][host]['name']}.key",  
+                    },
+                    "ulimits":{"memlock":{"soft": -1,"hard": -1}},
+                    "volumes": [f"{self.hosts_data['instances'][host]['name']}_data:/usr/share/elasticsearch/data", "certs:$CERTS_DIR"],
+                    "networks":["elastic"]}})
+            # Kibana nodes
+            if self.hosts_data['instances'][host]['name'] in self.hosts['kibana']:    
+                self.docker_compose['services'].update(
+                    {self.hosts_data['instances'][host]['name']:{
+                        "image":"docker.elastic.co/kibana/kibana:${VERSION}",
+                        "container_name":self.hosts_data['instances'][host]['name'],
+                        "depends_on": {self.hosts['elasticsearch'][0]:{"condition": "service_healthy"}},
+                        "ports":["5601:5601"],
+                        "environment":{
+                            "SERVERNAME": "localhost",
+                            "ELASTICSEARCH_URL": f"https://{self.hosts['elasticsearch'][0]}:9200",
+                            "ELASTICSEARCH_HOSTS": f"https://{self.hosts['elasticsearch'][0]}:9200",
+                            "ELASTICSEARCH_USERNAME": "kibana_system",
+                            "ELASTICSEARCH_PASSWORD": "CHANGEME",
+                            "ELASTICSEARCH_SSL_CERTIFICATEAUTHORITIES": "$CERTS_DIR/ca/ca.crt",
+                            "SERVER_SSL_ENABLED": "true",
+                            "SERVER_SSL_KEY": f"$CERTS_DIR/{self.hosts_data['instances'][host]['name']}/{self.hosts_data['instances'][host]['name']}.key",
+                            "SERVER_SSL_CERTIFICATE": f"$CERTS_DIR/{self.hosts_data['instances'][host]['name']}/{self.hosts_data['instances'][host]['name']}.crt"
+                        },
+                        "volumes":["certs:$CERTS_DIR"],
+                        "networks":["elastic"]
+                    }
+                    }
+                )
+        # Create docker-compose.yml
+        yaml = YAML()
+        yaml.explicit_start = True
+        yaml.indent(sequence=4, offset=2)
+        with open(fr'{directory_path}\docker-compose.yml', 'w') as file:
+            yaml.dump(self.docker_compose, file)
 
 
